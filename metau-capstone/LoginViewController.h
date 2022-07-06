872085739cb6ec79b5c738082f4fcaabe063e1b3
Shirley Zhu <shirleyzzhu@fb.com>
2022-07-06 10:01:28 -0700

    added view controllers to storyboard

---
+++ metau-capstone/LoginViewController.h
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.h
+//  metau-capstone
+//
+//  Created by Shirley Zhu on 7/1/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoginViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ metau-capstone/SignUpViewController.m
@@ -0,0 +1,31 @@
+//
+//  SignUpViewController.m
+//  metau-capstone
+//
+//  Created by Shirley Zhu on 7/1/22.
+//
+
+#import "SignUpViewController.h"
+
+@interface SignUpViewController ()
+
+@end
+
+@implementation SignUpViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
