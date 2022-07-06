c707135d51a23e9b8996b2fd4dcd72a7174f3da4
Shirley Zhu <shirleyzzhu@fb.com>
2022-07-06 15:20:32 -0700

    created bottom navigation bar

---
+++ metau-capstone/StudyViewController.h
@@ -0,0 +1,16 @@
+//
+//  StudyViewController.h
+//  metau-capstone
+//
+//  Created by Shirley Zhu on 7/6/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface StudyViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
