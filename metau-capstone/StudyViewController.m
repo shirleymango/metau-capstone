0d618721145d68d02618a3c4f16acacfece05e6b
Shirley Zhu <shirleyzzhu@fb.com>
2022-07-08 11:57:31 -0700

    added layer for card

--- metau-capstone/StudyViewController.m
+++ metau-capstone/StudyViewController.m
@@ -10,14 +10,29 @@
 #import "SceneDelegate.h"
 #import "LoginViewController.h"
 @interface StudyViewController ()
-
+@property (nonatomic, strong) CALayer *layer;
+@property (nonatomic, strong) CABasicAnimation *rotateAnim;
 @end
 
 @implementation StudyViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.layer = [[CALayer alloc] init];
+    self.layer.frame = CGRectMake(0, 0, 300, 180);
+    self.layer.backgroundColor = [[UIColor blackColor] CGColor];
+    self.layer.position = CGPointMake(self.view.center.x, self.view.center.y - 50);
+    [self.view.layer addSublayer:self.layer];
+    
+    self.rotateAnim = [CABasicAnimation animationWithKeyPath:@"transform.rotation"];
+    self.rotateAnim.fromValue = [NSNumber numberWithFloat:0];
+    self.rotateAnim.toValue = [NSNumber numberWithFloat:(360 * M_PI / 180.0f)];
+    self.rotateAnim.duration = 2.5;
+}
+
+- (IBAction)didTapScreen:(UITapGestureRecognizer *)sender {
+    [self.layer addAnimation:self.rotateAnim forKey:@"rotationAnimation"];
+//    self.layer.transform = CATransform3DMakeRotation(M_PI, 0, 1, 0);
 }
 
 /*
