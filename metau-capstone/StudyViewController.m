a47082c122a734d7837ad6c266001bf550b789c4
Shirley Zhu <shirleyzzhu@fb.com>
2022-07-11 14:02:30 -0700

    clicking right button increases the level

--- metau-capstone/StudyViewController.m
+++ metau-capstone/StudyViewController.m
@@ -95,6 +95,16 @@
 }
 
 - (IBAction)didTapRight:(UIButton *)sender {
+    // Update level
+    PFQuery *query = [PFQuery queryWithClassName:@"Flashcard"];
+    Flashcard *card = self.arrayOfCards[self.counter];
+    // Retrieve the object by id
+    [query getObjectInBackgroundWithId:card.objectId
+                                 block:^(PFObject *card, NSError *error) {
+        [card incrementKey:@"levelNum"];
+        [card saveInBackground];
+    }];
+    
     self.counter++;
     [self loadFlashcard];
 }
