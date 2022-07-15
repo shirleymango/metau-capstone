24b8abb415b46aa28059555d8f4181f932b1c653
Shirley Zhu <shirleyzzhu@fb.com>
2022-07-15 11:25:47 -0700

    change colors

--- metau-capstone/ScheduleViewController.m
+++ metau-capstone/ScheduleViewController.m
@@ -56,7 +56,7 @@
 - (nonnull __kindof UICollectionViewCell *)collectionView:(nonnull UICollectionView *)collectionView cellForItemAtIndexPath:(nonnull NSIndexPath *)indexPath {
     ScheduleCell *cell = [self.scheduleCollection dequeueReusableCellWithReuseIdentifier:@"ScheduleCollectionCell" forIndexPath:indexPath];
     cell.dayNum.text = [NSString stringWithFormat:@"%ld", indexPath.row+1];
-    cell.layer.borderColor = [UIColor blueColor].CGColor;
+    cell.layer.borderColor = [UIColor blackColor].CGColor;
     cell.layer.borderWidth = 1;
     return cell;
 }
