24d21602b7654c612aebb429c70a6992faae868d
Shirley Zhu <shirleyzzhu@fb.com>
2022-07-26 22:27:35 -0700

    create preview view controller

---
+++ metau-capstone/View Controllers/PreviewViewController.h
@@ -0,0 +1,16 @@
+//
+//  PreviewViewController.h
+//  metau-capstone
+//
+//  Created by Shirley Zhu on 7/26/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PreviewViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ metau-capstone/View Controllers/PreviewViewController.m
@@ -0,0 +1,31 @@
+//
+//  PreviewViewController.m
+//  metau-capstone
+//
+//  Created by Shirley Zhu on 7/26/22.
+//
+
+#import "PreviewViewController.h"
+
+@interface PreviewViewController ()
+
+@end
+
+@implementation PreviewViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
